af645eeeffec50b2a19002ceb96030fbd8b1f644
adityaholkar <aditya.holkar17@vit.edu>
2021-06-28 09:00:57 +0530

    POST method

--- demo/src/main/java/com/springboot/ad/topic/TopicController.java
+++ demo/src/main/java/com/springboot/ad/topic/TopicController.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,4 +24,11 @@ public class TopicController {
     public Topic getById(@PathVariable int id){
         return topicService.getTopic(id);
     }
+
+    @RequestMapping(method = RequestMethod.POST , value = "/topics")
+    public void postTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+
 }
--- demo/src/main/java/com/springboot/ad/topic/TopicService.java
+++ demo/src/main/java/com/springboot/ad/topic/TopicService.java
@@ -1,16 +1,17 @@
 package com.springboot.ad.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
-    private List<Topic> topics=Arrays.asList(
+    private List<Topic> topics=new ArrayList<>(Arrays.asList(
         new Topic(1,"Adam ASa",45),
         new Topic(2,"ajsbf jasbnd",45),
         new Topic(3,"jordan",70)
-        );
+        ));
     
     public List<Topic> getAllTopic() {
         return topics;
@@ -21,4 +22,8 @@ public class TopicService {
                     .findFirst()
                     .get();
     }
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+    
 }
