183b05c27803f2c0e34b837ded38949164f9fb05
adityaholkar <aditya.holkar17@vit.edu>
2021-06-30 08:36:41 +0530

    spring security config

--- security/src/main/java/com/springsecurity/security/SecurityConfiguration.java
+++ security/src/main/java/com/springsecurity/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.springsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -13,9 +14,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // TODO Auto-generated method stub
         auth.inMemoryAuthentication()
+            .withUser("user")
+            .password("user")
+            .roles("USER")
+            .and()
             .withUser("admin")
             .password("admin")
-            .roles("USER");
+            .roles("ADMIN");
     }
     
 
@@ -23,5 +28,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO Auto-generated method stub
+        http.authorizeRequests()
+            .antMatchers("/","static/css","static/js").permitAll()
+            .antMatchers("/user").hasAnyRole("ADMIN","USER")
+            .antMatchers("/admin").hasRole("ADMIN")
+            .and().formLogin();
+    }
+    
     
 }
